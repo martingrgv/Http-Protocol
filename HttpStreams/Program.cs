e48c6a824ad4361fbf5257298718efa49238938f
martin.grgv <martin.geor04@gmail.com>
2025-08-16 17:19:17 +0300

    initial commit

---
+++ HttpStreams/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+using var fileStream = new FileStream("message.txt", FileMode.Open);
+var lines = GetLinesChannel(fileStream);
+
+foreach (var line in lines)
+{
+    Console.WriteLine($"read: {line}");
+}
+
+
+static IEnumerable<string> GetLinesChannel(Stream stream)
+{
+    int bufferSize = 8;
+    byte[] buffer = new byte[bufferSize];
+
+    List<string> lines = [];
+    var sb = new StringBuilder();
+
+    while (stream.Read(buffer, 0, bufferSize) > 0)
+    {
+        string bufferText = Encoding.UTF8.GetString(buffer);
+        var newLineIndex = bufferText.IndexOf('\n');
+
+        if (newLineIndex > -1)
+        {
+            var endOfLineText = bufferText[..newLineIndex];
+            sb.Append(endOfLineText);
+
+            lines.Add(sb.ToString());
+
+            sb.Clear();
+            sb.Append(bufferText.AsSpan(newLineIndex + 1));
+
+            continue;
+        }
+
+        sb.Append(bufferText);
+    }
+
+    return lines.AsEnumerable();
+}
---
+++ Tcp/Program.cs
@@ -0,0 +1,66 @@
+﻿// Run in terminal to test:
+// printf "Do you have what it takes to be an engineer at TheStartup™?\r\n" | nc -w 1 127.0.0.1 40569
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var ipEndPoint = new IPEndPoint(IPAddress.Loopback, 40569);
+var tcpListener = new TcpListener(ipEndPoint);
+
+tcpListener.Start();
+Console.WriteLine("[SERVER] Listening on 127.0.0.1:40569");
+
+while (true)
+{
+    try
+    {
+
+        var tcpClient = await tcpListener.AcceptTcpClientAsync();
+        Console.WriteLine("[SERVER] Client connected");
+
+        NetworkStream stream = tcpClient.GetStream();
+        var lines = GetLinesChannel(stream);
+
+        foreach (var line in lines)
+        {
+            Console.WriteLine($"[SERVER] Recieved: {line}");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"[ERROR] {ex.Message}");
+    }
+}
+
+static IEnumerable<string> GetLinesChannel(Stream stream)
+{
+    int bufferSize = 8;
+    byte[] buffer = new byte[bufferSize];
+
+    List<string> lines = [];
+    var sb = new StringBuilder();
+
+    while (stream.Read(buffer, 0, bufferSize) > 0)
+    {
+        string bufferText = Encoding.UTF8.GetString(buffer);
+        var newLineIndex = bufferText.IndexOf('\n');
+
+        if (newLineIndex > -1)
+        {
+            var endOfLineText = bufferText[..newLineIndex];
+            sb.Append(endOfLineText);
+
+            lines.Add(sb.ToString());
+
+            sb.Clear();
+            sb.Append(bufferText.AsSpan(newLineIndex + 1));
+
+            continue;
+        }
+
+        sb.Append(bufferText);
+    }
+
+    return lines.AsEnumerable();
+}
